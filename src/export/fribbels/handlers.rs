f207dddeb6fa85db7f24bc00cf080a5e137d24d6
emmachase <me@its-em.ma>
2026-01-07 18:41:17 -0800

    chore: fmt

--- src/export/fribbels/handlers.rs
+++ src/export/fribbels/handlers.rs
@@ -274,22 +274,19 @@ impl OptimizerExporter {
 
     pub fn handle_gacha_info(&mut self, gacha_info: GetGachaInfoScRsp) {
         for banner in gacha_info.gacha_info_list {
-            self.banners.insert(
-                banner.gacha_id,
-                BannerInfo {
-                    rate_up_item_list: banner.item_detail_list,
-                    banner_type: match banner.gacha_id {
-                        1001 => BannerType::Standard,
-                        _ => {
-                            if self.is_lightcone(*banner.prize_item_list.first().unwrap()) {
-                                BannerType::LightCone
-                            } else {
-                                BannerType::Character
-                            }
+            self.banners.insert(banner.gacha_id, BannerInfo {
+                rate_up_item_list: banner.item_detail_list,
+                banner_type: match banner.gacha_id {
+                    1001 => BannerType::Standard,
+                    _ => {
+                        if self.is_lightcone(*banner.prize_item_list.first().unwrap()) {
+                            BannerType::LightCone
+                        } else {
+                            BannerType::Character
                         }
-                    },
+                    }
                 },
-            );
+            });
         }
     }
 
